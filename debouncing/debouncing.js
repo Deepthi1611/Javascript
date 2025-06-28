f6271d371e4a226864d8346baec688905883ef7f
deepthi1611 <deepthipurijala@gmail.com>
2025-06-28 11:16:58 +0530

    throttling

--- debouncing/debouncing.js
+++ debouncing/debouncing.js
@@ -24,3 +24,17 @@ const debounce = function(getData, delay) {
 // It's also used for infinite scrolling, window resize etc.
 
 const betterFunction = debounce(getData, 300);
+
+// Throttling:
+// Baby: Mom give me a piece of chocolate cake
+// Mom: No you can get one, only after 1 hour
+// (baby wont get a piece of cake no matter how many times she asked, but only after each hour)
+
+// Debouncing:
+// Baby: Mom give me a piece of chocolate cake, . . .Mom give me a piece of chocolate cake ... mom give me ...
+// Mom: No, You will get a piece of cake only after 1 hour from LAST time you asked for one.
+
+
+// In case of window resize, if we want to understand how frequently user is resizing the window then
+// throttling would make more sense, but if we want to understand user behavior/user pattern - how many times user is resizing the window
+// then debouncing would make more sense.
